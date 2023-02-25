f553926b4123dc9a18067f00c62571257c2160c5
Paul Mason <63802301+PaulMasonDev@users.noreply.github.com>
2023-02-25 14:41:50 -0800

    Update character.js

--- character.js
+++ character.js
@@ -1,87 +1,77 @@
 class Character {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.speed = 5;
-        this.sprite = document.createElement('div');
-        this.sprite.classList.add('character');
-        document.body.appendChild(this.sprite);
-    }
-    moveUp() {
-        this.y -= this.speed;
-        this.sprite.style.top = `${this.y}px`;
-    }
-    moveDown() {
-        this.y += this.speed;
-        this.sprite.style.top = `${this.y}px`;
-    }
-    moveLeft() {
-        this.x -= this.speed;
-        this.sprite.style.left = `${this.x}px`;
-    }
-    moveRight() {
-        this.x += this.speed;
-        this.sprite.style.left = `${this.x}px`;
-    }
-    handleSwipe(direction) {
-        switch (direction) {
-            case 'up':
-                this.moveUp();
-                break;
-            case 'down':
-                this.moveDown();
-                break;
-            case 'left':
-                this.moveLeft();
-                break;
-            case 'right':
-                this.moveRight();
-                break;
-        }
-    }
-}
-const character = new Character();
-document.addEventListener('keydown', (event) => {
-    switch (event.key) {
+  private x: number;
+  private y: number;
+  private speed: number;
+  private sprite: HTMLDivElement;
+
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.speed = 5;
+    this.sprite = document.createElement('div');
+    this.sprite.classList.add('character');
+    document.body.appendChild(this.sprite);
+    this.addEventListeners();
+  }
+
+  public moveUp(): void {
+    this.y -= this.speed;
+    this.sprite.style.top = `${this.y}px`;
+  }
+
+  public moveDown(): void {
+    this.y += this.speed;
+    this.sprite.style.top = `${this.y}px`;
+  }
+
+  public moveLeft(): void {
+    this.x -= this.speed;
+    this.sprite.style.left = `${this.x}px`;
+  }
+
+  public moveRight(): void {
+    this.x += this.speed;
+    this.sprite.style.left = `${this.x}px`;
+  }
+
+  private addEventListeners(): void {
+    document.addEventListener('keydown', (event) => {
+      switch (event.key) {
         case 'ArrowUp':
-            character.moveUp();
-            break;
+          this.moveUp();
+          break;
         case 'ArrowDown':
-            character.moveDown();
-            break;
+          this.moveDown();
+          break;
         case 'ArrowLeft':
-            character.moveLeft();
-            break;
+          this.moveLeft();
+          break;
         case 'ArrowRight':
-            character.moveRight();
-            break;
+          this.moveRight();
+          break;
+      }
+    });
+
+    const upButton = document.getElementById('up-button');
+    if (upButton) {
+      upButton.addEventListener('click', () => this.moveUp());
     }
-});
-let xStart = 0;
-let yStart = 0;
-document.addEventListener('touchstart', (event) => {
-    xStart = event.touches[0].clientX;
-    yStart = event.touches[0].clientY;
-});
-document.addEventListener('touchend', (event) => {
-    const xEnd = event.changedTouches[0].clientX;
-    const yEnd = event.changedTouches[0].clientY;
-    const xDiff = xEnd - xStart;
-    const yDiff = yEnd - yStart;
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-        if (xDiff > 0) {
-            character.handleSwipe('right');
-        }
-        else {
-            character.handleSwipe('left');
-        }
+
+    const downButton = document.getElementById('down-button');
+    if (downButton) {
+      downButton.addEventListener('click', () => this.moveDown());
     }
-    else {
-        if (yDiff > 0) {
-            character.handleSwipe('down');
-        }
-        else {
-            character.handleSwipe('up');
-        }
+
+    const leftButton = document.getElementById('left-button');
+    if (leftButton) {
+      leftButton.addEventListener('click', () => this.moveLeft());
     }
-});
+
+    const rightButton = document.getElementById('right-button');
+    if (rightButton) {
+      rightButton.addEventListener('click', () => this.moveRight());
+    }
+  }
+}
+
+const character = new Character();
